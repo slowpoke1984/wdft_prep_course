410beafcfbcff93c7dd1ac78305050245be5ed38
slowpoke1984 <seantran.st1984@gmail.com>
2017-03-22 21:18:40 +0000

    assignment 2 completed

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,7 +2,9 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
+for(var i = 1; i <=100; i++){
+    console.log(i);
+};
 
 
 /*
@@ -17,3 +19,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+
+var hashtg = '#';
+for (i = 0; i < 7; i++) {
+   console.log(hashtg);
+   hashtg += '#';
+}  
