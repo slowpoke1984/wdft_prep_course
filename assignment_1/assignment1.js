4a51e3be9ad67c1172828008c03aecfc486b2bd5
slowpoke1984 <seantran.st1984@gmail.com>
2017-04-03 20:12:48 +0000

    completed assignment 1

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -10,7 +10,7 @@ Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
+for(var i=0; i<11; i++){
     //use the array push() method here.
 numbers.push(i);
 }
